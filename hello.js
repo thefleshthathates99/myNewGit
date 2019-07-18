3f4d9ae049016ff5605977044411be4bcc2898a4
Zane Stevens <zane.stevens@griffithuni.edu.au>
2019-07-18 11:34:17 +1000

    Adding function into branch

--- hello.js
+++ hello.js
@@ -1,1 +1,13 @@
+
+
+function addStuff(x,y){
+    return x + y;
+}
+
 console.log("Hello GitWorld");
+let firstNumber = 5;
+let secondNumber = 8;
+console.log(firstNumber);
+console.log(secondNumber);
+
+console.log(addStuff (firstNumber, secondNumber));
